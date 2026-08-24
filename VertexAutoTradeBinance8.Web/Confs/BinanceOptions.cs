fadcb972f49ee7313b2009d15cf293f50a155ac5
Hrach <hrachk@mail.ru>
2026-08-24 22:39:51 +0400

    GIBRID-11V initial

--- VertexAutoTradeBinance8.Web/Confs/BinanceOptions.cs
+++ VertexAutoTradeBinance8.Web/Confs/BinanceOptions.cs
@@ -3,6 +3,6 @@
     public class BinanceOptions
     {
         public string ApiKey { get; set; } = string.Empty;
-        public string SecretKey { get; set; } = string.Empty; 
+        public string SecretKey { get; set; } = string.Empty;
     }
 }
